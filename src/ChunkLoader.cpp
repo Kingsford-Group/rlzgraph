574de6a6cc6274074f2d3bb37ce5269ce7d9279c
YutongQ <yutongq@andrew.cmu.edu>
2021-05-25 14:34:33 -0400

    ChunkLoader ready to go

--- src/ChunkLoader.cpp
+++ src/ChunkLoader.cpp
@@ -3,10 +3,16 @@
 #include <vector>
 #include <ios>
 #include <chrono>
+#include <assert.h>
 
 using namespace std;
 using namespace std::chrono;
 
+enum chunkStatus{
+    ENDSTR = -1,
+    ENDFILE = -2,
+    EXCEEDNUMBER = -3
+};
 
 void printArray(vector<char> arr){
     cout << "[";
@@ -26,13 +32,12 @@ class ChunkLoader{
         int strIdx;
         int numSeq;
         int refIdx;
-        
-        int currStr;
+
         int currpos;
 
         ifstream inputFile;
 
-        ChunkLoader(string fname, int numSeq, int refIdx, int bufSize):inputFile(fname),buf((size_t)bufSize, (char)-1){
+        ChunkLoader(string fname, int numSeq, int refIdx, int bufSize):inputFile(fname),buf((size_t)bufSize, (char)ENDFILE){
             this->bufSize = bufSize;
             this->bufIdx = 0;
             this->strIdx = -1;
@@ -46,54 +51,50 @@ class ChunkLoader{
 
         char next(bool newStart){            
             string line;
-            // cout << "In next currpos0 " << currpos << endl;
 
             // chunk has not been loaded yet
-            if ((bufIdx == 0 && buf[bufIdx] == -1) || bufIdx == bufSize){
-                // while (!inputFile.eof()){                    
-
-                    // skip all the content for the reference string
-                    while (strIdx == refIdx){
-                        getline(inputFile, line);
-                        if (line[0]=='>'){
-                            strIdx += 1;
-                            line.clear();
-                        }
-                    }
+            if ((bufIdx == 0 && buf[bufIdx] == ENDFILE) || bufIdx == bufSize){
 
                     // check if exceeds numSeq
                     if (strIdx == numSeq){
-                        return -1;
+                        cout << "Exceeds number of sequence!" << endl;
+                        return EXCEEDNUMBER;
                     }
 
                     currpos = inputFile.tellg();
                     getline(inputFile, line);
-                    // cout << "In next currpos1 " << currpos << endl;
 
-
-                    // read a line and see if it is a '>'
-                    // if so -- increment strIdx and return -1
+                    // check for the start of a new sequence
                     if (line[0] == '>'){
                         if (!newStart){
                             inputFile.seekg(currpos, ios_base::beg);                            
                             return -1;
                         } else{
                             strIdx += 1;
+
+                            // skip reference sequence
+                            while (strIdx == refIdx){
+                                getline(inputFile, line);
+                                if (line[0] =='>'){
+                                    strIdx += 1;
+                                    line.clear();
+                                }
+                            }
                             currpos = inputFile.tellg();
                         }
                     }
-                    // else -- go back to the start of the line and load buffer
+
+                    // if not ">", go back to the start of the line and load buffer
                     else{
                         inputFile.seekg(currpos, ios_base::beg);
                     }
-                    // cout << "In next currpos2 " << currpos << endl;
 
                     if (currpos != -1){
-                        fill(buf.begin(), buf.end(), -1);
+                        fill(buf.begin(), buf.end(), ENDFILE);
                         inputFile.read(&buf[0], bufSize);
                     }
                     else{
-                        return -1;
+                        return ENDFILE;
                     }
                     
                     bufIdx = 0; 
@@ -103,38 +104,37 @@ class ChunkLoader{
                     } else{
                         return buf[bufIdx++];
                     }
-                    
-                // }
             } 
+
+            // if ">" is in the middle of the buffer
             else if (buf[bufIdx] == '>'){
-                // strIdx += 1;
+
+                // let file starts at ">"
                 currpos += bufIdx;
-                // cout << "In next currpos3 " << currpos << endl;
-                // cout << "In next currpos4 " << inputFile.tellg() << endl;
-                // cout << "EOF? "<< inputFile.eof() << endl;
                 bufIdx = bufSize;
+
                 if (inputFile.eof())
                     inputFile.clear();
                 inputFile.seekg(currpos, inputFile.beg);
-                // cout << "In next currpos5 " << inputFile.tellg() << endl;
-
-                return -1;
+                return ENDSTR;
             } 
             else{
+                // skip "\n" character
                 if (buf[bufIdx] == '\n'){
                     bufIdx ++;
                     return next(false);
                 }
+                // either returns next character of end of file enum
                 return buf[bufIdx++];
             }
         }
 };
 
 int main(){
-    string fname = "/home/yutongq/RLZGraph/rlzgraph/test/ecoli_O157_5_1.fasta";
-    int numSeq = 5;
-    int refIdx = 6;
-    int bufSize = 500;
+    string fname = "/home/yutongq/RLZGraph/rlzgraph/test/ecoli_O157_50_1.fasta";
+    int numSeq = 50;
+    int refIdx = 60;
+    int bufSize = 5000;
     ChunkLoader loader(fname, numSeq, refIdx, bufSize);
     bool newStart = true; 
     int counter = 0;
@@ -142,34 +142,42 @@ int main(){
     auto start = chrono::high_resolution_clock::now();
     char cc = 0;
 
-    string ofname_1 = "/home/yutongq/RLZGraph/rlzgraph/test/chunk_test_1.txt";
-    string ofname_2 = "/home/yutongq/RLZGraph/rlzgraph/test/chunk_test_2.txt";
+    // string ofname_1 = "/home/yutongq/RLZGraph/rlzgraph/test/chunk_test_1.txt";
+    // string ofname_2 = "/home/yutongq/RLZGraph/rlzgraph/test/chunk_test_2.txt";
 
-    ofstream f1(ofname_1);
-    ofstream f2(ofname_2);
+    // ofstream f1(ofname_1);
+    // ofstream f2(ofname_2);
     
-    for (int i=0; i<numSeq; i++){
+    while (true){
         string s;
+        int innerCounter = 0;
+        char c;
         while(true){
-            char c = loader.next(newStart);
-            // cout << "[" << loader.buf <<"]" << endl;
-            // cout << "Position: " << loader.currpos << endl;
+            c = loader.next(newStart);
             if (newStart)
                 newStart = false;
-            if (c != -1)
-                s += c;
-                // counter += 1;
+            if (c > 0){
+                // s += c;
+                innerCounter += 1;
+            }
             else break;
-            // counter += 1;
-            // if (counter > 200){
-            //     cout << "Infinite loop??" << endl;
-            //     break;
-            // }
         }
-        f1 << s << endl;
-        // f1 << endl << "SeqID" << loader.strIdx << endl;
+
         newStart = true;
-        cout << "SeqID " << loader.strIdx << endl;
+        // cout << "[" << loader.buf <<"]" << endl;
+        // cout << "c " << (int)c << endl; 
+        if (c == ENDFILE || c == EXCEEDNUMBER){
+            cout << "status " << (int)c << endl;
+            break;
+        }
+        // cout << "SeqID " << loader.strIdx << endl;
+        // cout << loader.bufIdx << endl;
+        // assert(loader.strIdx < 49);
+
+        // cout << "currpos " << loader.currpos << endl;
+        // cout << innerCounter << endl;
+        // // f1 << s << endl;
+        // // f1 << endl << "SeqID" << loader.strIdx << endl;
     }
 
     auto stop = high_resolution_clock::now();
@@ -184,20 +192,27 @@ int main(){
     ifstream input(fname);
     string currString;
     while(getline(input, line)){
+        counter +=1;
         if (line[0] != '>'){
             currString += line;
             line.clear();
         } else if (currString != ""){
             strings.push_back(currString);
-            f2 << currString << endl;
-            cout << currString.length() << endl;
+            // f2 << currString << endl;
+            // cout << currString.length() << endl;
             currString.clear();
             line.clear();
         }
     }
-    strings.push_back(currString);
-    f2 << currString << endl;
-    cout << currString.length() << endl;
+
+    for (string s : strings){
+        for (char c : s){
+            counter += 1;
+        }
+    }
+    // strings.push_back(currString);
+    // f2 << currString << endl;
+    // cout << currString.length() << endl;
     stop = high_resolution_clock::now();
     duration = duration_cast<microseconds>(stop-start);
     cout << "Time taken by default "<< duration.count() << " microseconds." << endl;
